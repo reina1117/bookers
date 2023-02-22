59e65ae1453339a38ed99f3da59499d739f84bca
reina1117 <122454145+reina1117@users.noreply.github.com>
2023-02-22 11:51:52 +0000

    [Add] create、update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash[:alret] = "Book was not successfully created"
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -24,10 +30,10 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
-    book.destroy 
+    book.destroy
     redirect_to '/books'
   end
 
